77a7bc0569753caa95f54f8456f0c0f9e93bcdde
Artem <kuzovlev181207@gmail.com>
2026-07-26 01:41:26 +0300

    Add player leaderstats and death tracking
    This script initializes player leaderstats including Coins, Level, Kills, and Deaths. It also tracks player deaths when the humanoid dies.

--- src/ServerScript.lua
+++ src/ServerScript.lua
@@ -1,1 +1,39 @@
+local Players = game:GetService("Players")
 
+Players.PlayerAdded:Connect(function(player)
+
+	local leaderstats = Instance.new("Folder")
+	leaderstats.Name = "leaderstats"
+	leaderstats.Parent = player
+
+	local Coins = Instance.new("IntValue")
+	Coins.Name = "Coins"
+	Coins.Value = 0
+	Coins.Parent = leaderstats
+
+	local Level = Instance.new("IntValue")
+	Level.Name = "Level"
+	Level.Value = 1
+	Level.Parent = leaderstats
+
+	local Kills = Instance.new("IntValue")
+	Kills.Name = "Kills"
+	Kills.Value = 0
+	Kills.Parent = leaderstats
+
+	local Deaths = Instance.new("IntValue")
+	Deaths.Name = "Deaths"
+	Deaths.Value = 0
+	Deaths.Parent = leaderstats
+
+	player.CharacterAdded:Connect(function(character)
+
+		local humanoid = character:WaitForChild("Humanoid")
+
+		humanoid.Died:Connect(function()
+			Deaths.Value += 1
+		end)
+
+	end)
+
+end)
